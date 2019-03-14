0cec462c2baa4a3af0f5ec435faaa954acec3c14
Yurii Rebryk <y.a.rebryk@gmail.com>
2019-03-13 22:18:34 -0700

    Remove task02

--- task02/mdp_get_action_value.py
+++
@@ -1,9 +0,0 @@
-
-def get_action_value(mdp, state_values, state, action, gamma):
-    """ Computes Q(s,a) as in formula above """
-    q = 0
-    
-    for state_prime, p in mdp.get_next_states(state, action).items():
-         q += p * (mdp.get_reward(state, action, state_prime) + gamma * state_values[state_prime])
-
-    return q
