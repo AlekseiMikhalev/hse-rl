f41c7157be05b9ba7b3c79a6db7fa2c34c0d8d8c
Yurii Rebryk <y.a.rebryk@gmail.com>
2019-03-18 20:39:24 -0700

    Add task04

---
+++ task04/replay_buffer.py
@@ -0,0 +1,68 @@
+import random
+
+import random
+import numpy as np
+
+from collections import deque
+
+
+class ReplayBuffer(object):
+    def __init__(self, size):
+        """
+        Create Replay buffer.
+        Parameters
+        ----------
+        size: int
+            Max number of transitions to store in the buffer. When the buffer
+            overflows the old memories are dropped.
+
+        Note: for this assignment you can pick any data structure you want.
+              If you want to keep it simple, you can store a list of tuples of (s, a, r, s') in self._storage
+              However you may find out there are faster and/or more memory-efficient ways to do so.
+        """
+        self._storage = deque()
+        self._maxsize = size
+
+    def __len__(self):
+        return len(self._storage)
+
+    def add(self, obs_t, action, reward, obs_tp1, done):
+        '''
+        Make sure, _storage will not exceed _maxsize. 
+        Make sure, FIFO rule is being followed: the oldest examples has to be removed earlier
+        '''
+        data = (obs_t, action, reward, obs_tp1, done)
+
+        # add data to storage
+        self._storage.append(data)
+        
+        if len(self._storage) == self._maxsize + 1:
+            self._storage.popleft()
+
+    def sample(self, batch_size):
+        """Sample a batch of experiences.
+        Parameters
+        ----------
+        batch_size: int
+            How many transitions to sample.
+        Returns
+        -------
+        obs_batch: np.array
+            batch of observations
+        act_batch: np.array
+            batch of actions executed given obs_batch
+        rew_batch: np.array
+            rewards received as results of executing act_batch
+        next_obs_batch: np.array
+            next set of observations seen after executing act_batch
+        done_mask: np.array
+            done_mask[i] = 1 if executing act_batch[i] resulted in
+            the end of an episode and 0 otherwise.
+        """
+        indices = list(range(len(self._storage)))
+        idxes = np.random.choice(indices, size=batch_size)
+        
+        data = [self._storage[it] for it in idxes]
+        states, actions, rewards, next_states, is_done = zip(*data)
+
+        return np.array(states), np.array(actions), np.array(rewards), np.array(next_states), np.array(is_done)
